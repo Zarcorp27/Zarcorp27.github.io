55ebf283b178b7b7c24d3cf64cb2021f9aaca759
jona27081 <zs20006736@estudiantes.uv.mx>
2022-09-04 18:51:02 -0500

    create work-experience #2

--- src/app/work-experiencie/work-experiencie.component.ts
+++ src/app/work-experiencie/work-experiencie.component.ts
@@ -7,9 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperiencieComponent implements OnInit {
 
+  workExperience : Array<any> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    let work1 = {
+            fecha: "2022-2022",
+            ubicacion: "Potrero Nuevo Ver.",
+            puesto: "Repartidor",
+            empresa: "Importadoras Joaquin",
+            logros : [
+                {descripcion : "Rutas completadas" },
+                {descripcion : "Abastecer locales de productos antes de hora de apertura"}
+            ]
+    };
+
+    let work2 = {
+            fecha: "2021-2022",
+            ubicacion: "Potrero Nuevo Ver.",
+            puesto: "Tutor de regularizacion",
+            empresa: "Conalep",
+            logros : [
+                {descripcion : "Aumento de remuneracion"},
+                {descripcion : "Por cada alumno que se logre regularizar, recibera un compensacion extra"}
+            ]
+    };
+    
+    let work3 = {
+            fecha: "2021-2022",
+            ubicacion: "Potrero Nuevo Ver.",
+            puesto: "Repartidor",
+            empresa: "La Parrilla Game",
+            logros : [
+                {descripcion : "Rutas completadas" },
+                {descripcion : "Abastecer locales de productos antes de hora de apertura"}
+            ]
+    };  
+
+    let work4 = {
+            fecha: "2021-2022",
+            ubicacion: "Potrero Nuevo Ver.",
+            puesto: "Desarrollador de Sistemas",
+            empresa: "La parrilla Game",
+            logros : [
+                {descripcion : "Sistema en Linea" },
+                {descripcion : "El sistema funciona estable y correctamente"}
+            ]
+    };          
+    
+
+    this.workExperience.push(work1);
+    this.workExperience.push(work2);
+    this.workExperience.push(work3);
+    this.workExperience.push(work4);
+
   }
 
 }
